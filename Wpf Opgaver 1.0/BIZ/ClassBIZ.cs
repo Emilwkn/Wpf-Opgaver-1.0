346260ad912cb91e4a6334da93b7ce20e9143111
Emil Weigelt Kjær Nielsen <emil601b@edu.campusvejle.dk>
2019-12-10 09:01:07 +0100

    Add project files.

---
+++ Wpf Opgaver 1.0/BIZ/ClassBIZ.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Wpf_Opgaver_1._0.Biz
+{
+    class ClassBIZ
+    {
+
+        public void Delopgave12(ListBox listBox)
+        {
+            listBox.Items.Clear();
+
+            for (int i= 4711; i < 4737; i++)
+            {
+                listBox.Items.Add(i.ToString());
+            }
+        }
+
+        public void Delopgave13(ListBox listBox)
+        {
+            Random random = new Random();
+
+            listBox.Items.Clear();
+
+            for (int i = 0; i < 25; i++)
+            {
+                int randomTal = random.Next(100000, 1000001);
+                listBox.Items.Add(randomTal.ToString());
+            }
+        }
+
+    }
+}
---
+++ Wpf Opgaver 1.0/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf_Opgaver_1._0.Biz;
+
+namespace Wpf_Opgaver_1._0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClassBIZ classBIZ = new ClassBIZ();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOpg12_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave12(listBoxRes);
+        }
+
+        private void ButtonOpg13_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave13(listBoxRes);
+        }
+    }
+}
